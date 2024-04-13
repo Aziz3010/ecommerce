4ea9c774e24fc4071443502933455f8ffa2880b7
ahmed abdelaziz <a.abdelazizg@gmail.com>
2024-04-13 15:08:37 +0200

    HeaderBasket

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -3,15 +3,18 @@ import Logo from "../../../assets/svg/cart.svg?react";
 import { useAppSelector } from "../../../store/hook";
 import { getCartTotalQuantitySelector } from "../../../store/cart/selectors";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const { basketContainer, basketQuantity, pumpBasketQuantity } = styles;
+const { basketContainer, basketQuantity, pumpBasketQuantity, basketCart } =
+  styles;
 
 const HeaderBasket = () => {
+  const navigate = useNavigate();
   const totalQuantity: number = useAppSelector(getCartTotalQuantitySelector);
   const [isAnimate, setIsAnimate] = useState<boolean>(false);
 
   useEffect(() => {
-    if(!totalQuantity) return;
+    if (!totalQuantity) return;
     setIsAnimate(true);
     const debounce = setTimeout(() => {
       setIsAnimate(false);
@@ -20,11 +23,14 @@ const HeaderBasket = () => {
   }, [totalQuantity]);
 
   return (
-    <div className={basketContainer}>
-      <Logo title="basket icon" />
-      <div className={`${basketQuantity} ${isAnimate && pumpBasketQuantity}`}>
-        {totalQuantity}
+    <div className={basketContainer} onClick={() => navigate("/cart")}>
+      <div className={basketCart}>
+        <Logo title="basket icon" />
+        <div className={`${basketQuantity} ${isAnimate && pumpBasketQuantity}`}>
+          {totalQuantity}
+        </div>
       </div>
+      <h3>Cart</h3>
     </div>
   );
 };
